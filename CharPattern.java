a208a79ec7ba0a52ce91cccfb3a4c880b17be001
Tauseef Jamal <121728120+Tauseef670@users.noreply.github.com>
2023-01-23 13:25:38 +0530

    Add files via upload

---
+++ CharPattern.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class CharPattern {
+    public static void main(String[] args) {
+        
+        char i,j;
+
+        for(i='A'; i<='E';i++)
+        {
+            for(j='A';j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println("\n");
+        }
+    }
+    
+}
