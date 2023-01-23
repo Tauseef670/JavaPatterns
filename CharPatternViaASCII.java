7140e8df146164258cd6adff58dd0808da4188db
Tauseef Jamal <121728120+Tauseef670@users.noreply.github.com>
2023-01-23 13:28:23 +0530

    Add files via upload

---
+++ CharPatternViaASCII.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class CharPatternViaASCII {
+    public static void main(String[] args) {
+        
+
+        char i,j;
+
+        for(i=65; i<=69;i++)
+        {
+            for(j=65;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println("\n");
+        }
+    }
+    
+}
