573c5393ed71830553de44988a362d250f59ba15
Tauseef Jamal <121728120+Tauseef670@users.noreply.github.com>
2023-01-23 13:10:03 +0530

    Add files via upload
    Increasing Number Pattern
    
    1
    
    12
    
    123
    
    1234
    
    12345

---
+++ NumberPattern.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        
+        int i,j;
+        for(i=1;i<=5;i++)  // Rows
+        {
+            for(j=1;j<=i;j++) // Columns
+            {
+                System.out.print(j);
+            }
+            System.out.println("\n");
+        }
+    }
+    
+}
