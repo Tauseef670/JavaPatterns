19aa32e511bc3ee76c6d88e03fb68793b81d3c2c
Tauseef Jamal <121728120+Tauseef670@users.noreply.github.com>
2023-01-24 15:17:10 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,35 @@
+package Java;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        
+        int i,j,k;
+
+        for(i=1;i<=5;i++)  // Rows---
+        {
+            for(j=5;j>i;j--)  // Space/ column
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=(2*i-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+
+        for(i=4;i>=1;i--)  // Rows---
+        {
+            for(j=5;j>i;j--)  // Space/ column
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=(2*i-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+    
+}
